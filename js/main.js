ee744d793f07c630fb283f6fcdc99f0d41e5d73e
Lucas Neumann <lucas.neumann@nubank.com.br>
2017-04-22 02:11:53 -0300

    fix link

--- js/main.js
+++ js/main.js
@@ -8,16 +8,30 @@ $.fn.extend({
     }
 });
 
-// SMOOTH SCROLLING
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 600);
-});
+
+
 
 $( document ).ready(function() {
+	// SMOOTH SCROLLING
+	var hashTagActive = "";
+		 $(".scroll").on("click touchstart" , function (event) {
+				 if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+						 event.preventDefault();
+						 //calculate destination place
+						 var dest = 0;
+						 if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+								 dest = $(document).height() - $(window).height();
+						 } else {
+								 dest = $(this.hash).offset().top;
+						 }
+						 //go to destination
+						 $('html,body').animate({
+								 scrollTop: dest
+						 }, 1000, 'swing');
+						 hashTagActive = this.hash;
+				 }
+		 });
 
 	// HIDE ALL ANIMATED ELEMENTS
 	$('.animated').css('opacity', '0')
