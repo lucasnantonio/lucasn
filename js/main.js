67344706dd6a2b8e78f607dfe5c0bb1c3fca89a7
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-03-16 17:30:54 -0300

    date and skills

--- js/main.js
+++ js/main.js
@@ -1,95 +1,44 @@
-// CREATE ANIMATECSS FUNCTION
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
+document.addEventListener("DOMContentLoaded", function(event) {
+  getSkillDiv();
+  getMonthsDiv();
+
+  setInterval(changeSkill, 2000);
+  getMonths();
 });
 
+let skillDiv;
+let hiredMonths;
+let skillList = [
+  'research',
+  'explore',
+  'design',
+  'validate',
+  'define',
+  'prototype',
+  'test',
+  'ship',
+  'write copy to',
+]
 
-$( document ).ready(function() {
-	// SMOOTH SCROLLING
-	var hashTagActive = "";
-		 $(".scroll").on("click touchstart" , function (event) {
-				 if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-						 event.preventDefault();
-						 //calculate destination place
-						 var dest = 0;
-						 if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-								 dest = $(document).height() - $(window).height();
-						 } else {
-								 dest = $(this.hash).offset().top;
-						 }
-						 //go to destination
-						 $('html,body').animate({
-								 scrollTop: dest
-						 }, 1000, 'swing');
-						 hashTagActive = this.hash;
-				 }
-		 });
+let getSkillDiv = function () {
+  skillDiv = document.getElementById('skills');
+}
 
-	// HIDE ALL ANIMATED ELEMENTS
-	$('.animated').css('opacity', '0')
+let getMonthsDiv = function () {
+  hiredMonths = document.getElementById('months');
+}
 
-// SECTION ONE
-	var sectionOne = new Waypoint({
-	  element: document.getElementById('sectionOne'),
-	  handler: function(direction) {
-			if (direction == 'down'){
-				$('.animated-sectionOne').css('opacity', '1')
-				$('.animated-sectionOne').animateCss('fadeInUp')
-			}
-			this.destroy()
-	  }
-	})
+let i = 0;
+function changeSkill() {
+  skillDiv.innerHTML = skillList[i];
+  i = (i + 1) % skillList.length;
+}
 
-// SECTION TWO
-	var sectionTwo = new Waypoint({
-	  element: document.getElementById('sectionTwo'),
-	  handler: function(direction) {
-			if (direction == 'down'){
-				$('.animated-sectionTwo').css('opacity', '1')
-				$('.animated-sectionTwo').animateCss('fadeInUp')
-			}
-			this.destroy()
-	  },
-		offset: '10%'
-	})
-
-	// SECTION THREE
-		var sectionThree = new Waypoint({
-		  element: document.getElementById('sectionThree'),
-		  handler: function(direction) {
-				if (direction == 'down'){
-					$('.animated-sectionThree').css('opacity', '1')
-					$('.animated-sectionThree').animateCss('fadeInUp')
-				}
-				this.destroy()
-		  },
-			offset: '10%'
-		})
-
-// ARROW
-	$( '#homeArrow').mouseenter(
-		function(e) {
-			$(this).animateCss('shakeUp');
-		}
-	);
-
-// Profile bubble
-// $( '#profile-front').css('')
-$( '#profile-front').mouseenter(
-	function(e) {
-		$('#profile-back').css('opacity', '1')
-		$('#profile-back').animateCss('zoomIn');
-	}
-);
-$( '#profile-front').mouseleave(
-	function(e) {
-		$('#profile-back').animateCss('zoomOut');
-		$('#profile-back').css('opacity', '0')
-	}
-);
-});
+let getMonths = function(){
+  let today = new Date();
+  let hiredDate = new Date("April 08, 2016 01:15:00");
+  let months = (today.getFullYear() - hiredDate.getFullYear())*12
+  months-= hiredDate.getMonth() + 1;
+  months+= today.getMonth() +1;
+  hiredMonths.innerHTML = months <= 0 ? 0 : months;
+}
